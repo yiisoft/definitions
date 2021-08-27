b19e4b5c52af10de5460148bc14e545b8ad43c33
Sergei Predvoditelev <sergey.predvoditelev@gmail.com>
2021-08-27 23:21:07 +0300

    Move `ReferencesArray` and `DynamicReferencesArray` from `yiisoft/di`

---
+++ tests/Unit/ReferencesArrayTest.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Definitions\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use Yiisoft\Definitions\DynamicReference;
+use Yiisoft\Definitions\DynamicReferencesArray;
+use Yiisoft\Definitions\Exception\InvalidConfigException;
+use Yiisoft\Definitions\Reference;
+use Yiisoft\Definitions\ReferencesArray;
+
+final class ReferencesArrayTest extends TestCase
+{
+    public function testReferencesArray()
+    {
+        $ids = ['key1' => 'first', 'key2' => 'second'];
+
+        $references = ReferencesArray::from($ids);
+
+        $this->assertInstanceOf(Reference::class, $references['key1']);
+        $this->assertInstanceOf(Reference::class, $references['key2']);
+    }
+
+    public function testReferencesArrayFail()
+    {
+        $ids = ['first', 22];
+
+        $this->expectException(InvalidConfigException::class);
+        $references = ReferencesArray::from($ids);
+    }
+
+    public function testDynamicReferencesArray()
+    {
+        $ids = ['key1' => 'first', 'key2' => 'second'];
+
+        $references = DynamicReferencesArray::from($ids);
+
+        $this->assertInstanceOf(DynamicReference::class, $references['key1']);
+        $this->assertInstanceOf(DynamicReference::class, $references['key2']);
+    }
+
+    public function testDynamicReferencesArrayFail()
+    {
+        $ids = ['first', 22];
+
+        $this->expectException(InvalidConfigException::class);
+        DynamicReferencesArray::from($ids);
+    }
+}
