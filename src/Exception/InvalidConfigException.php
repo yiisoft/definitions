3c601507af18e414c163d21b3d922a887778c78a
Dmitrii Derepko <xepozz@list.ru>
2022-11-09 22:17:46 +0300

    Remove extending InvalidConfigException with ContainerExceptionInterface

--- src/Exception/InvalidConfigException.php
+++ src/Exception/InvalidConfigException.php
@@ -5,11 +5,10 @@ declare(strict_types=1);
 namespace Yiisoft\Definitions\Exception;
 
 use Exception;
-use Psr\Container\ContainerExceptionInterface;
 
 /**
  * `InvalidConfigException` is thrown when definition configuration is not valid.
  */
-final class InvalidConfigException extends Exception implements ContainerExceptionInterface
+final class InvalidConfigException extends Exception
 {
 }
