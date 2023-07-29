4ced43c489dc278f805fd41ef8c3f36c395c0988
Dmitrii Derepko <xepozz@list.ru>
2023-07-29 17:38:34 +0300

    Fix tests and psalm errors

--- src/LazyDefinition.php
+++ src/LazyDefinition.php
@@ -7,26 +7,40 @@ namespace Yiisoft\Definitions;
 use ProxyManager\Factory\LazyLoadingValueHolderFactory;
 use Psr\Container\ContainerInterface;
 use Yiisoft\Definitions\Contract\DefinitionInterface;
-use Yiisoft\Di\Helpers\DefinitionNormalizer;
+use Yiisoft\Definitions\Helpers\Normalizer;
 
 final class LazyDefinition implements DefinitionInterface
 {
     public function __construct(
         private mixed $definition,
+        /**
+         * @var class-string
+         */
         private string $objectClass,
     ) {
     }
 
+    /**
+     * @psalm-suppress MixedArgumentTypeCoercion
+     */
     public function resolve(ContainerInterface $container): mixed
     {
+        /** @var LazyLoadingValueHolderFactory $factory */
         $factory = $container->get(LazyLoadingValueHolderFactory::class);
+        /**
+         * @var mixed $definition
+         */
         $definition = $this->definition;
         $objectClass = $this->objectClass;
 
+        /** @psalm-suppress InvalidArgument */
         return $factory->createProxy(
             $objectClass,
-            function (&$wrappedObject) use ($container, $objectClass, $definition) {
-                $definition = DefinitionNormalizer::normalize($definition, $objectClass);
+            function (mixed &$wrappedObject) use ($container, $objectClass, $definition) {
+                $definition = Normalizer::normalize($definition, $objectClass);
+                /**
+                 * @var mixed $wrappedObject
+                 */
                 $wrappedObject = $definition->resolve($container);
             }
         );
