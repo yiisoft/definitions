cbc4373e10659d84aa432ae82e35d837b95a8277
Dmitrii Derepko <xepozz@list.ru>
2023-08-01 23:42:13 +0300

    Add a hack for already normalized configs

--- src/ArrayDefinition.php
+++ src/ArrayDefinition.php
@@ -69,7 +69,8 @@ final class ArrayDefinition implements DefinitionInterface
         return new self(
             $config[self::CLASS_NAME],
             $config[self::CONSTRUCTOR] ?? [],
-            self::getMethodsAndPropertiesFromConfig($config)
+            // Already normalized config. Ignore all the rest parameters
+            $config['methodsAndProperties'] ?? self::getMethodsAndPropertiesFromConfig($config),
         );
     }
 
