cd2b1de2226e0f2e45ebba101e4e4b702b4a9f1a
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-02-18 10:58:40 +0300

    fix

--- src/Helpers/DefinitionValidator.php
+++ src/Helpers/DefinitionValidator.php
@@ -345,12 +345,12 @@ final class DefinitionValidator
         }
 
         $modifiers = $property->getModifiers();
+
+        /**
+         * @pslam-suppress UndefinedConstant, MixedOperand Needs for PHP 8.3 or lower
+         */
         return !($modifiers & ReflectionProperty::IS_PRIVATE_SET
             || $modifiers & ReflectionProperty::IS_PROTECTED_SET
-        )
-
-
-
-         ;
+        );
     }
 }
