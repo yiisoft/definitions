1b077bcb4757b44bac71f66c595242dd455b88fc
Максим Спирков <spirkov.2001@mail.ru>
2026-01-14 12:54:29 +0400

    configs

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -14,9 +14,24 @@ $finder = (new Finder())
     ->exclude('Php8_4/');
 
 return (new Config())
+    ->setRiskyAllowed(true)
     ->setParallelConfig(ParallelConfigFactory::detect())
     ->setRules([
-        '@PER-CS2.0' => true,
+        '@PER-CS3.0' => true,
         'no_unused_imports' => true,
+        'ordered_class_elements' => true,
+        'class_attributes_separation' => ['elements' => ['method' => 'one']],
+        'declare_strict_types' => true,
+        'native_function_invocation' => true,
+        'native_constant_invocation' => true,
+        'fully_qualified_strict_types' => [
+            'import_symbols' => true
+        ],
+        'global_namespace_import' => [
+            'import_classes' => true,
+            'import_constants' => true,
+            'import_functions' => true,
+        ],
     ])
     ->setFinder($finder);
+
